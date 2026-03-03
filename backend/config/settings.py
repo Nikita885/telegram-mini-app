d003b313ab1030f19504d18a11d869b2897eb855
Nikita885 <faikqweas@mail.ru>
2026-03-03 14:29:17 +0500

    Исправил баги

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'database-tg-app'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'Qiwe1419as'),
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),  # <--- важное
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
